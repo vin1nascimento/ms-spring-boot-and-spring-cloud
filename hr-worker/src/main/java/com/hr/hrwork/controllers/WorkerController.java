9710070902a8769a99e2fbdf92d2a9238beece6a
vin1nascimento <vini988nascimento@gmail.com>
2023-02-07 08:58:26 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/hr/hrwork/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/hr/hrwork/controllers/WorkerController.java
@@ -2,6 +2,8 @@ package com.hr.hrwork.controllers;
 
 import com.hr.hrwork.repositories.WorkerRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/workers")
 @RequiredArgsConstructor
 public class WorkerController {
 
     private final WorkerRepository repository;
+    private final Environment env;
 
     @GetMapping
     public ResponseEntity<List<com.hr.hrwork.entities.Worker>> findAll() {
@@ -25,6 +29,9 @@ public class WorkerController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<com.hr.hrwork.entities.Worker> findById( @PathVariable int id) {
+
+        log.info("PORT = " + env.getProperty("local.server.port"));
+
         com.hr.hrwork.entities.Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
