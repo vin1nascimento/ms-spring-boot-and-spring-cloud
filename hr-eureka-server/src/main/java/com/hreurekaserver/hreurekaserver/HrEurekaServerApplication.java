f97a727201dda304ddc2a6627fb9a6c21d604f63
vin1nascimento <vini988nascimento@gmail.com>
2023-02-13 19:05:59 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/main/java/com/hreurekaserver/hreurekaserver/HrEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.hreurekaserver.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaServerApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( HrEurekaServerApplication.class, args );
+    }
+
+}
